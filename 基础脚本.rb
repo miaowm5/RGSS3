f23ce68cc02bb6fefd6a5276764104695849a0bf
mwm5 <miaowm5@users.noreply.github.com>
2014-11-07 18:26:44 +0800

    Update 基础脚本.rb

--- 基础脚本.rb
+++ 基础脚本.rb
@@ -6,27 +6,73 @@
 #==============================================================================
 
 $m5script ||= {}
-$m5script["M5Base"] = $m5script[:M5Base] = 20140916
+$m5script["M5Base"] = $m5script[:M5Base] = 20141107
 #--------------------------------------------------------------------------
 # ● 版本检查
 #
 #     M5script.version(ver) 检查脚本版本是否高于指定版本
 #--------------------------------------------------------------------------
 module M5script
-  def self.version(ver,hint = "喵呜喵5基础脚本版本过低！",key = "M5Base", h2="")
-    raise(h2) unless $m5script[key]
-    raise(hint) if (ver > $m5script[key])
+  def self.version(ver,hint = "喵呜喵5基础脚本版本过低！",key = :M5Base, h2="")
+    version = $m5script[key.to_sym] || $m5script[key.to_s] || raise(h2)
+    raise(hint) if (ver > version)
   end
 end
 #--------------------------------------------------------------------------
-# ● 读取备注的基本方法
-#     note     : 备注文字
+# ● 匹配文本内容
+#
+#     M5script.match_text(text, note, default, value, list)
+#
+#     text     : 文本内容
+#     note     : 需要匹配的文字
 #     default  : 缺省值
+#     value    : 匹配文字是否需要包含结果
+#     list     : 是否返回全部匹配结果的数组
+#--------------------------------------------------------------------------
+module M5script
+  def self.match_text(text, note, default, value, list)
+    all_result = []
+    text.each_line do |line|
+      line.chomp!
+      if value
+        result = /^\s*<\s*#{note}\s+(\S+)\s*>\s*$/ =~ line ? $1 : nil        
+      else
+        result = /^\s*<\s*#{note}\s*>\s*$/ =~ line ? true : nil        
+      end
+      all_result.push result if result
+    end
+    all_result.push default if all_result.size == 0
+    return list ? all_result : all_result[0]  
+  end
+end
+#--------------------------------------------------------------------------
+# ● 读取备注
+#
+#     m5note(note, default, value, list)
 #--------------------------------------------------------------------------
 class RPG::BaseItem
-  def m5note(note,default = nil)
-    result = /<\s*#{note}\s+(\S+)\s*>/ =~ @note ? $1.to_s : default
-    result
+  def m5note(note, default = nil, value = true, list = false)
+    M5script.match_text(@note, note, default, value, list)
+  end
+end
+#--------------------------------------------------------------------------
+# ● 读取活动事件页的第一条指令，如果为注释则进行匹配
+#
+#     M5script.read_event_note(map, id, note, default, value, list)
+#--------------------------------------------------------------------------
+module M5script
+  def self.read_event_note(map,id,note,default = nil,value = true,list = false)
+    ev = load_data(sprintf("Data/Map%03d.rvdata2", map)).events[id]
+    return default unless ev
+    page = Game_Event.new(map,ev)
+    page.refresh
+    return default if page.empty?
+    text = ""
+    page.list.each do |command|
+      break if command.code != 108 && command.code != 408
+      text += command.parameters[0] + "\n"
+    end
+    M5script.match_text(text, note, default, value, list)    
   end
 end
 #--------------------------------------------------------------------------
@@ -69,48 +115,6 @@ class Scene_Base
   end
 end
 #--------------------------------------------------------------------------
-# ● 绘制EXP槽
-#
-#     m5_draw_exp_gauge
-#--------------------------------------------------------------------------
-class Window_Base
-  def m5_draw_exp_gauge(actor, x, y, width = 124, vocab = "Exp")
-    now_exp = actor.exp - actor.current_level_exp
-    next_exp = actor.next_level_exp - actor.current_level_exp    
-    draw_gauge(x, y, width, now_exp.to_f/next_exp, m5_exp_gauge_color1, 
-      m5_exp_gauge_color2)
-    change_color(system_color)
-    draw_text(x, y, 30, line_height, vocab)
-    draw_current_and_max_values(x, y, width, now_exp, next_exp,
-      m5_exp_color(actor), normal_color)
-  end
-  def m5_exp_gauge_color1;tp_gauge_color1;end
-  def m5_exp_gauge_color2;tp_gauge_color2;end
-  def m5_exp_color(actor);tp_color(actor);end
-end
-#--------------------------------------------------------------------------
-# ● 绘制多排图标
-#
-#     m5_draw_icons(icons, x, y, width, col, enabled) 描绘图标
-#     m5_draw_actor_icons(actor, x, y, width, col)    描绘角色状态图标
-#--------------------------------------------------------------------------
-class Window_Base  
-  def m5_draw_icons(icons, x, y, width = 96, col = 1, enabled = [])    
-    col.times do |line|
-      temp_icons = icons[width * line / 24, width / 24]
-      return unless temp_icons
-      temp_icons.each_with_index do |icon_index, pos|
-        alpha = enabled[pos + width * line / 24]
-        draw_icon(icon_index, x + 24 * pos, y + 24 * line, !alpha)
-      end
-    end
-  end
-  def m5_draw_actor_icons(actor, x, y, width = 96, col = 1)
-    icons = (actor.state_icons + actor.buff_icons)
-    m5_draw_icons(icons, x, y, width, col)
-  end
-end
-#--------------------------------------------------------------------------
 # ● 获取控制符的参数（这个方法会破坏原始数据）
 #
 #     m5_obtain_escape_param(text)
@@ -121,38 +125,78 @@ class Window_Base
   end
 end
 #--------------------------------------------------------------------------
+# ● 绘制值槽
+#
+#     include M5script::M5_Window_Gauge
+#
+#     m5_draw_gauge(now, max, vocab, color, x, y, width)
+#     color[0]  槽值渐变色
+#     color[1]  槽值渐变色
+#     color[2]  槽值名称颜色
+#     color[3]  槽值当前值颜色
+#     color[4]  槽值最大值颜色
+#--------------------------------------------------------------------------
+module M5script;module M5_Window_Gauge
+  def m5_draw_gauge(now, max, vocab, color, x, y, width = 124)
+    draw_gauge(x, y, width, [now.to_f/max,1].min, color[0], color[1])
+    change_color(color[2])
+    draw_text(x, y, 30, line_height, vocab)    
+    draw_current_and_max_values(x, y, width, now, max, color[3], color[4])
+  end
+end;end
+#--------------------------------------------------------------------------
+# ● 绘制图标
+#
+#     include M5script::M5_Window_Icons
+#
+#     m5_draw_icons(icons, x, y, width, col, enabled) 多行描绘图标
+#--------------------------------------------------------------------------
+module M5script;module M5_Window_Icons
+  def m5_draw_icons(icons, x, y, width = 96, col = 1, enabled = [])    
+    col.times do |line|
+      temp_icons = icons[width * line / 24, width / 24]
+      return unless temp_icons
+      temp_icons.each_with_index do |icon_index, pos|
+        alpha = enabled[pos + width * line / 24]
+        draw_icon(icon_index, x + 24 * pos, y + 24 * line, !alpha)
+      end
+    end
+  end
+end;end
+#--------------------------------------------------------------------------
 # ● 字体大小调整
+#
+#     include M5script::M5_Window_FontSize
 #
 #     m5_font_width       默认文字宽度
 #     m5_font_height      默认文字高度
 #     m5_font_size_change 调整文字大小
 #--------------------------------------------------------------------------
-class Window_Base
+module M5script;module M5_Window_FontSize
+  def m5_font_size_change(width = m5_font_width, height = m5_font_height)    
+    contents.font.size -= 1 while text_size("口").width > width if width    
+    contents.font.size -= 1 while text_size("口").height > height if height    
+  end
+  def m5_font_width;  return nil; end
+  def m5_font_height; return nil; end
+end;end
+class Window_Base  
   alias m5_20140728_reset_font_settings reset_font_settings
   def reset_font_settings
     m5_20140728_reset_font_settings
-    m5_font_size_change
+    m5_font_size_change if respond_to?(:m5_font_size_change)
   end
   alias m5_20140728_create_contents create_contents
   def create_contents
     m5_20140728_create_contents
-    m5_font_size_change
+    m5_font_size_change if respond_to?(:m5_font_size_change)
   end
-  def m5_font_size_change(width = m5_font_width, height = m5_font_height)    
-    if width
-      contents.font.size -= 1 while text_size("口").width > width
-    end
-    if height
-      contents.font.size -= 1 while text_size("口").height > height
-    end
-  end
-  def m5_font_width;  return nil; end
-  def m5_font_height; return nil; end
 end
 #--------------------------------------------------------------------------
 # ● Window_M5CalText
 #
 #     计算一段文字大小时使用的临时窗口
+#
 #     cal_all_text_height(text) 计算总高度
 #     cal_all_text_width(text)  计算最大宽度
 #     calc_line_width(text)     计算单行宽度
@@ -161,9 +205,8 @@ end
 #     line_height               设置每行文字的高度
 #--------------------------------------------------------------------------
 class Window_M5CalText < Window_Base
-  attr_writer :font_width
-  attr_writer :font_height
-  attr_writer :line_height
+  include M5script::M5_Window_FontSize
+  attr_writer :font_width, :font_height, :line_height
   def initialize
     super(0, 0, Graphics.width, Graphics.height)
     self.visible = false
@@ -175,9 +218,8 @@ class Window_M5CalText < Window_Base
   def m5_font_width;@font_width;end
   def m5_font_height;@font_height;end
   def line_height
-    if @line_height then return @line_height
-    elsif @font_height then return @font_height    
-    end
+    return @line_height if @line_height
+    return @font_height if @font_height
     super
   end
   def cal_all_text_height(text)
@@ -198,7 +240,7 @@ class Window_M5CalText < Window_Base
     end
     return all_text_width
   end
-  def calc_line_width(text)    
+  def calc_line_width(text)
     reset_font_settings
     pos = {:x => 0, :y => 0, :new_x => 0, :height => Graphics.height}
     process_character(text.slice!(0, 1), text, pos) until text.empty?
@@ -208,40 +250,17 @@ class Window_M5CalText < Window_Base
   def draw_text(*args);end
 end
 #--------------------------------------------------------------------------
-# ● 可以设置位置、大小的类原生帮助窗口
+# ● 可以设置位置、大小的帮助窗口
 #
 #     Window_M5Help
 #--------------------------------------------------------------------------
-class Window_M5Help < Window_Base
+class Window_M5Help < Window_Help
   def initialize(line_number = 2, x = 0, y = 0, width = Graphics.width, 
       height = fitting_height(line_number))
-    super(x,y,width,height)
-  end
-  def set_text(text)
-    if text != @text
-      @text = text
-      refresh
-    end
-  end
-  def clear;set_text("");end
-  def set_item(item);set_text(item ? item.description : "");end
-  def refresh
-    contents.clear
-    draw_text_ex(4, 0, @text)
-  end
-end
-#--------------------------------------------------------------------------
-# ● 获取指定地图特定事件名的事件ID数组
-#
-#     M5script.match_ev_name(name,id)
-#--------------------------------------------------------------------------
-module M5script
-  def self.match_ev_name(match = "m5",map_id = $game_map.map_id)
-    array = []
-    map = load_data(sprintf("Data/Map%03d.rvdata2", map_id))
-    map.events.each_pair {|id,ev| array.push(id) if ev.name == match}
-    array
-  end
+    super
+    self.x, self.y ,self.width, self.height = x, y, width, height    
+    create_contents
+  end  
 end
 #--------------------------------------------------------------------------
 # ● 字体的设置
@@ -253,20 +272,14 @@ class Font
   def m5_return_all_setting
     set = [self.name,self.size,self.bold,self.italic,self.outline,self.shadow,
       "Color.new#{self.color}","Color.new#{self.out_color}"]
-    instance_variables.each do |var|
-      set.push instance_variable_get(var)
-    end
-    set
+    instance_variables.each {|var| set.push instance_variable_get(var) }
+    return set
   end
   def m5_set_all_setting(set)
-    instance_variables.reverse!.each do |var|
-      instance_variable_set(var, set.pop)
-    end
-    list = ["self.name","self.size","self.bold","self.italic","self.outline",
-      "self.shadow","self.color","self.out_color"]
-    list.reverse!.each do |var|
-      eval("#{var}=#{set.pop}")
-    end
+    instance_variables.reverse!.each {|var| instance_variable_set(var, set.pop)}    
+    list = %w[self.name self.size self.bold self.italic self.outline 
+      self.shadow self.color self.out_color]
+    list.reverse!.each {|var| eval("#{var}=#{set.pop}") }    
   end
 end
 #--------------------------------------------------------------------------
@@ -282,7 +295,7 @@ module M5script
   end
 end
 #--------------------------------------------------------------------------
-# ● 打开网页
+# ● 打开地址
 #
 #     M5script.open_url(addr)
 #--------------------------------------------------------------------------
