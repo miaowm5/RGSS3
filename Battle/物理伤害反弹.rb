a3080ec03298bd0d1465929a9a61a0580d4b008b
miaowm5 <miaowm5@gmail.com>
2015-10-20 23:21:20 +0800

    add script

---
+++ Battle/物理伤害反弹.rb
@@ -0,0 +1,76 @@
+=begin
+===============================================================================
+  物理伤害反弹 By喵呜喵5
+===============================================================================
+
+【说明】
+
+  当满足条件时，角色原本应该发动的物理反击效果会变成物理反弹
+
+  物理反弹效果和魔法反射效果相同，
+  是指敌人攻击自己时，不仅无法攻击自己同时还会受到来自自己的攻击
+  与物理反击效果下的主角攻击敌人不同，物理反弹效果是敌人自己攻击自己
+  （即：用敌人的能力来攻击敌人自己，数据库伤害公式中的b全部被替换成a）
+
+=end
+$m5script ||= {}
+$m5script[:M5Reflect20151020] = 20151022
+module M5Reflect20151020
+#==============================================================================
+#  设定部分
+#==============================================================================
+
+  HINT = '%s反弹了物理伤害！'
+
+  # 这里设置物理反弹时的提示文字
+  # %s 表示对象的名字
+
+  STATE = [10, 11, 12, 13]
+
+  # 当角色处于上面数字对应ID的状态时，物理反击的效果变成物理反弹的效果
+
+  WEAPON = [6, 12, 18, 24]
+
+  # 当角色装备上面数字对应ID的武器时，物理反击的效果变成物理反弹的效果
+
+  ARMOR = [15, 50]
+
+  # 当角色装备面数字对应ID的防具时，物理反击的效果变成物理反弹的效果
+
+  FORMULA = 'a.atk - a.def + (%s)'
+
+  # 如果希望物理伤害反弹时伤害公式也发生改变的话，可以在这里设置新的公式
+  # 公式的设置格式和数据库中的伤害公式相同，不过a、b指的都是受到攻击者
+  # %s 表示原本的伤害公式
+  # 不需要改变伤害公式的话，这里直接设置成 '%s' 即可
+
+#==============================================================================
+#  设定结束
+#==============================================================================
+  def self.check(actor)
+    armor = actor.armors.collect {|armors| armors.id }
+    return true unless (armor | ARMOR).size == armor.size + ARMOR.size
+    state = actor.states.collect {|state| state.id }
+    return true unless (state | STATE).size == state.size + STATE.size
+    weapon = actor.weapons.collect {|weapons| weapons.id }
+    return true unless (weapon | WEAPON).size == weapon.size + WEAPON.size
+    false
+  end
+end
+class Scene_Battle
+  alias m5_20131123_invoke_counter_attack invoke_counter_attack
+  def invoke_counter_attack(target, item)
+    if M5Reflect20151020.check(target)
+      item = item.clone
+      item.damage = item.damage.clone
+      item.damage.formula = sprintf(M5Reflect20151020::FORMULA, item.damage.formula)
+      Sound.play_evasion
+      @log_window.add_text sprintf(M5Reflect20151020::HINT, target.name)
+      @log_window.wait
+      @log_window.back_one
+      apply_item_effects(@subject, item)
+    else
+      m5_20131123_invoke_counter_attack(target, item)
+    end
+  end
+end
--- Message/头像批量修改.rb
+++ Message/头像批量修改.rb
@@ -7,55 +7,76 @@
 
   指定的开关打开或者队伍的队长为特定ID的角色时，
   对话中显示头像功能显示的指定头像将批量替换成其他头像
-  
-  修改的只是头像素材的文件名而已，头像的位置（这个素材中的第几个头像）并不会改变  
+
+  修改的只是头像素材的文件名而已，头像的位置（这个素材中的第几个头像）并不会改变
   如果连位置都要修改的话……自己给我用PS去调整啦！
-  
+
 =end
-$m5script ||= {};$m5script[:M5MF20140805] = 20141111
-module M5MF20140805;SWI = [
+$m5script ||= {};$m5script[:M5MF20140805] = 20151021
+module M5MF20140805
 #==============================================================================
 # 设定部分
-#==============================================================================  
-  
+#==============================================================================
+
+  SWI = [
+
+    [1,"Actor1","Spiritual"],
+    [2,"Actor2","Spiritual"],
+
+  ]
   # 设置特定开关打开时需要进行替换的头像，设置格式：
   #  [开关的编号, 需要替换的头像文件名, 用于替换的头像文件名],
-  # (文件名的前后需要加上英文的双引号，请不要忘记每条设置最后的逗号)  
-  
-  [1,"Actor1","Spiritual"],
-  [2,"Actor2","Spiritual"],
-  
-  
-  ];LEADER = [ # 请不要删除本行  
-  
+  # (文件名的前后需要加上英文的双引号，请不要忘记每条设置最后的逗号)
+
+  LEADER = [
+
+    [1,"Actor1","Spiritual"],
+    [2,"Actor2","Spiritual"],
+
+  ]
   # 设置队伍第一名成员为指定角色时需要进行替换的头像，设置格式：
   #  [第一名成员的角色ID, 需要替换的头像文件名, 用于替换的头像文件名],
-  # (文件名的前后需要加上英文的双引号，请不要忘记每条设置最后的逗号)
-  
-  [1,"Actor1","Spiritual"],
-  [2,"Actor2","Spiritual"],
-  
+
+  STRICT = true
+  # 设置为 false 时，脚本将关闭严厉模式
+  # 在严厉模式下，【只有】通过显示对话指令显示的脸图将被修改
+  # 不使用严厉模式时，其他地方显示的脸图也可能会发生改变（例如菜单中）
+
 #==============================================================================
 # 设定结束
 #==============================================================================
-  ]
+  def self.check(filename)
+    begin
+      proc = Proc.new do |list, condition|
+        list.each do |data|
+          next if filename != data[1]
+          next unless condition.call(data[0])
+          return data[2]
+        end
+      end
+      proc.call SWI, -> id { $game_switches[id] }
+      actor_id = $game_party.members[0].id
+      proc.call LEADER, -> id { actor_id == id }
+    rescue
+      p "头像批量修改脚本出现未知异常"
+    end
+    filename
+  end
 end
+if M5MF20140805::STRICT
 class Game_Interpreter
   alias m5_20140805_command_101 command_101
   def command_101
     @params = @params.clone
-    m5_20141111_cfn(M5MF20140805::SWI,
-      Proc.new{|id| $game_switches[id]})
-    m5_20141111_cfn(M5MF20140805::LEADER,
-      Proc.new{|id| $game_party.members[0].id == id})
-    m5_20140805_command_101    
+    @params[0] = M5MF20140805.check(@params[0])
+    m5_20140805_command_101
   end
-  def m5_20141111_cfn(list,proc)
-    list.collect{|set| set[0]}.each_with_index do |id,index|
-      if proc.call(id) && @params[0] == list[index][1]
-        @params[0] = list[index][2]
-        break
-      end
-    end    
+end
+else
+class << Cache
+  alias m5_20151020_face face
+  def face(filename)
+    m5_20151020_face M5MF20140805.check(filename)
   end
+end
 end
