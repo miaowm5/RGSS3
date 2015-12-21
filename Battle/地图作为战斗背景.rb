274e1d6bb8c351124faddfcb15d46c19fc77463d
miaowm5 <miaowm5@gmail.com>
2015-12-21 17:06:54 +0800

    no need base script, fix bitmap bug

--- Battle/地图作为战斗背景.rb
+++ Battle/地图作为战斗背景.rb
@@ -8,8 +8,7 @@
   使用地图作为战斗背景
 
 =end
-$m5script ||= {};raise("需要喵呜喵5基础脚本的支持") unless $m5script[:M5Base]
-$m5script[:M5BB20150211] = 20150211;M5script.version(20150211)
+$m5script ||= {}; $m5script[:M5BB20150211] = 20151221
 module M5BB20150211
 #==============================================================================
 #  设定部分
@@ -30,11 +29,11 @@ module M5BB20150211
 
   # 战斗背景是否模糊，true：模糊，false：不模糊
 
-  TONE = Tone.new(0,0,0,0)
+  TONE = [0,0,0,0]
 
   # 战斗背景的色调，四个数字分别代表R、G、B、灰度
 
-  COLOR = Color.new(0,0,0,70)
+  COLOR = [0,0,0,70]
 
   # 战斗背景的颜色，四个数字分别代表R、G、B、Alpha
 
@@ -45,6 +44,18 @@ module M5BB20150211
 #==============================================================================
 #  设定结束
 #==============================================================================
+  def self.snapshot
+    @background_bitmap.dispose if @background_bitmap
+    @background_bitmap = Graphics.snap_to_bitmap
+  end
+  def self.background; @background_bitmap; end
+end
+class Scene_Map
+  alias m5_20151221_pre_battle_scene pre_battle_scene
+  def pre_battle_scene
+    m5_20151221_pre_battle_scene
+    M5BB20150211.snapshot
+  end
 end
 class Spriteset_Battle
   alias m5_20150211_create_battleback1 create_battleback1
@@ -56,14 +67,15 @@ class Spriteset_Battle
       return if battleback2_name && battleback2_name != ""
     end
     source = M5BB20150211::BLUR ? SceneManager.background_bitmap :
-      SceneManager.m5_background_bitmap
-    bitmap = Bitmap.new(640, 480)
+      M5BB20150211.background
+    @back1_sprite.bitmap.dispose
+    bitmap = Bitmap.new(Graphics.width, Graphics.height)
     bitmap.stretch_blt(bitmap.rect, source, source.rect)
     @back1_sprite.bitmap = bitmap
     @back1_sprite.zoom_x = M5BB20150211::ZOOM
     @back1_sprite.zoom_y = M5BB20150211::ZOOM
-    @back1_sprite.tone.set(M5BB20150211::TONE)
-    @back1_sprite.color.set(M5BB20150211::COLOR)
+    @back1_sprite.tone.set(Tone.new *M5BB20150211::TONE)
+    @back1_sprite.color.set(Color.new *M5BB20150211::COLOR)
     center_sprite(@back1_sprite)
   end
 end
