ca9ba40a0322e0e4467f4bb8c45b584016d48743
miaowm5 <miaowm5@gmail.com>
2016-02-14 23:25:15 +0800

    fix bug press up button would select wrong choice when window has more than 2 choices

--- Message/防止错按选项.rb
+++ Message/防止错按选项.rb
@@ -10,8 +10,16 @@
   防止手贱党按键太快结果误选了列表中的第一个选项
 
 =end
-$m5script ||= {};$m5script[:M5NC20140915] = 20140915
+$m5script ||= {}; $m5script[:M5NC20140915] = 20160214
 class Window_ChoiceList
   alias m5_20140915_start start
-  def start;m5_20140915_start;unselect;end
+  def start
+    m5_20140915_start
+    unselect
+  end
+  alias m5_20160214_cursor_up cursor_up
+  def cursor_up(wrap = false)
+    @index < 0 && @index = 1
+    m5_20160214_cursor_up(wrap)
+  end
 end
